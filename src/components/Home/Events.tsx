10e780d45013508b785a6f400c167344dd46da06
Muhammad Annas Khan <annasking601@gmail.com>
2026-04-23 17:46:52 +0500

    adding

--- src/components/Home/Events.tsx
+++ src/components/Home/Events.tsx
@@ -1,8 +1,23 @@
+"use client"
 import React from 'react'
+import Marquee from "react-fast-marquee"
 
-const Events = () => {
+const Events: React.FC = () => {
+  const scrollText = "UPCOMING TOURS * UPCOMING TOURS * UPCOMING TOURS * UPCOMING TOURS * "
+
   return (
-    <div>Events</div>
+    <section className="relative z-20 pb-30 overflow-hidden select-none w-full max-w-full">
+      <Marquee 
+        speed={120} 
+        gradient={false} 
+        direction="left"
+        autoFill
+      >
+        <h2 className="text-[#AFD8FF] text-[15vw] overflow-y-hidden md:text-[12vw] font-black uppercase leading-none tracking-tighter whitespace-nowrap pr-10">
+          {scrollText}
+        </h2>
+      </Marquee>
+    </section>
   )
 }
 
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,6 +1,7 @@
 import About from '@/components/Home/About'
 import BestSongs from '@/components/Home/BestSongs'
 import Contact from '@/components/Home/Contact'
+import Events from '@/components/Home/Events'
 import Hero from '@/components/Home/Hero'
 import Projects from '@/components/Home/Projects'
 import Scroller from '@/components/Home/Scroller'
@@ -13,6 +14,7 @@ const Home = () => {
         <Projects />
         <Scroller />
         <BestSongs />
+        <Events />
         <Contact />
     </div>
   )
